30d51103123782f591daa977aa7f7ac50cc1ba83
unknown <sam.park.email@gmail.com>
2018-06-15 12:17:46 -0400

    late initial commit

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,7 +1,11 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
 1.  Describe Middleware, Sessions (as we know them in express), bcrypt and JWT.
+bycrypt is for hasing passwords. Middleware serves as a middleman of sorts, functions living between applications and dbs or any type of software. A session is a place to store information across requests, each user has a unique session. JWT or jsonwebtokens is a way to transfer information securely between parties.
+
 
 2.  What does bcrypt do in order to prevent attacks?
+It stores the password as a hash on the database.
 
 3.  What are the three parts of the JSON Web Token?
+    header, payload signature
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -5,9 +5,16 @@ const Schema = mongoose.Schema;
 const SALT_ROUNDS = 11;
 
 const UserSchema = Schema({
-  // create your user schema here.
-  // username: required, unique and lowercase
-  // password: required
+ username: {
+   type: String,
+   required: true,
+   lowercase: true,
+   unique: true,
+ },
+ password: {
+   type: String,
+   required: true,
+ }
 });
 
 UserSchema.pre('save', function(next) {
@@ -15,6 +22,13 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
+  bcrypt.hash(this.password, SALT_ROUNDS, (error, hash) => {
+    if (error) {
+      return next(error);
+    }
+    this.password = hash
+    return next();
+  })
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
@@ -22,6 +36,14 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  return bcrypt.compare(plainTextPW, callBack, function(err, res) {
+    if (res) {
+      console.log(res);
+    } else if (err){
+     console.log(err);
+    }
+
+  });
 };
 
 module.exports = mongoose.model('User', UserSchema);
